9f3da61c05dca36948889109284e425bbf663e82
Assunta Della Porta <a.dellaporta28@studenti.unisa.it>
2024-01-11 10:25:29 +0100

    sgd one dimention

--- Assia/python/sgd/sgd_1d.py
+++ Assia/python/sgd/sgd_1d.py
@@ -7,11 +7,11 @@ import matplotlib.pyplot as plot
 class sgd(): 
 
     def __init__(self):
-        self._mn = [0, 0]
         self._mp = [5, 5]
-        self._N = 2000  # numero di campioni 
+        self._mn = [0, 0]
+        self._N = 10000  # numero di campioni 
         self._sigma = 0.5  # varianza
-        self._u = 0.2   # step size
+        self._u = 0.9  # step size
         # inizializzazione di beta 
         self._b0 = random.uniform(-3, 3)
         self._b1 = random.uniform(-3, 3)
@@ -31,20 +31,21 @@ class sgd():
 
     def sgd(self, decreasing=False): 
         if decreasing == True: 
-            tau = 500
+            tau = 200
             self._u = tau
-        b0s = np.ones(self._dataset.shape[0])
+        b0s = np.ones(self._dataset.shape[0])  # inizializzazione dei vettori
         b1s = np.ones(self._dataset.shape[0])
         Js = np.ones(self._dataset.shape[0])
         for i in range(self._dataset.shape[0]): 
             sample = self._dataset[i] # estrazione di un campione 
             # aggiornamento del pesi 
-            grad = self._loss_grad(sample[0], sample[1], sample[2], self._b0, self._b1)
-            self._b0 = self._b0 - self._u*grad[0]
+            grad = self._loss_grad2(sample[0], sample[1], self._b0, self._b1)
+            self._b0 = self._b0 - self._u*grad[0]    # aggiornamento 
             self._b1 = self._b1 - self._u*grad[1]
             b0s[i] = self._b0
             b1s[i] = self._b1
-            Js[i] = 1 # self._cost()
+            # Js[i] = self._cost()
+            Js[i] = 1
             if decreasing == True: 
                 self._u = tau/(1+i)
         return b0s, b1s, Js
@@ -52,6 +53,8 @@ class sgd():
     def plot_results(self, b0s, b1s, Js):
         num_points = self._dataset.shape[0]
         plot.figure()
+        plot.xlabel('b0')
+        plot.ylabel('b1')
         plot.scatter(b0s, b1s, c=np.arange(num_points), cmap='viridis', alpha=0.7, edgecolors='w', linewidth=0.5)
         plot.savefig('scatter_weights.png')
 
@@ -65,10 +68,10 @@ class sgd():
 
 
     def _data_generation(self, m0, m1, sigma, N, label): 
-        data = np.zeros((N, 3)) # inzializzazione del vettore 
+        data = np.zeros((N, 2)) # inzializzazione del vettore 
         data[:, 0] = np.random.normal(m0, math.sqrt(sigma), (1, N))
-        data[:, 1] = np.random.normal(m1, math.sqrt(sigma), (1, N))
-        data[:, 2] = np.ones(N)*label
+        # data[:, 1] = np.random.normal(m1, math.sqrt(sigma), (1, N))
+        data[:, 1] = np.ones(N)*label
         # data[:, 2] = np.round(data[:, 2]).astype(int)
         # print(data)
         return data
@@ -80,6 +83,13 @@ class sgd():
         grad0 = factor*x0
         grad1 = factor*x1
         return [grad0, grad1]
+
+    def _loss_grad2(self, x, label, b0, b1): 
+        exp = np.exp(label*(b0+x*b1))
+        grad_b1 = -(label*x)/(exp+1)
+        grad_b0 = -label/(exp+1)
+        return [grad_b0, grad_b1]
+
     
     def _loss(self, x0, x1, label, b0, b1): 
         exponent = label*x0*b0 + label*x1*b1  # y*x^T*b
